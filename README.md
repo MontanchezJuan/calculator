2c0d3858304da93733c0a389ba4b42e2321dd6ef
MontanchezJuan <juanfelipemontanchez@gmail.com>
2023-08-25 11:08:35 -0500

    subtraction and division endpoints added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-## Calculator
+# Calculator
+
+## How to use?
+
+1. install development environment with
+
+   python -m venv venv
+
+2. install flask with
+
+   pyp install flask
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 # Endpoint para sumar
+# Recibe dos numeros de los cuales suma el primer número por el segundo
 @app.route('/calculator/addition', methods=['POST'])
 def addition():
     data = request.get_json()
@@ -14,7 +14,20 @@ def addition():
 
     return jsonify({"respuesta": f"El resultado de la suma es: {resp}"})
 
-# Endpoint para multiplicación 
+# Endpoint para restar
+# Recibe dos numeros de los cuales resta el segundo número al primer número
+@app.route('/calculator/subtraction', methods=['POST'])
+def subtraction():
+    data = request.get_json()
+    num1 = int(data['num1'])
+    num2 = int(data['num2'])
+
+    resp = num1 - num2
+
+    return jsonify({"respuesta": f"El resultado de la resta es: {resp}"})
+
+# Endpoint para multiplicación
+# Recibe dos numeros de los cuales multiplica el primer número por el segundo
 @app.route('/calculator/multiply', methods=['POST'])
 def multiply():
     data = request.get_json()
@@ -24,6 +37,22 @@ def multiply():
     resp = num1 * num2
 
     return jsonify({"respuesta": f"El resultado de la multipilcación es: {resp}"})
+
+# Endpoint para dividir 
+# Recibe dos numeros de los cuales divide el segundo número al primer número
+# Además se valida la división entre 0, si es así se retorna una respuesta diferente
+@app.route('/calculator/division', methods=['POST'])
+def division():
+    data = request.get_json()
+    num1 = int(data['num1'])
+    num2 = int(data['num2'])
+
+    if num2 == 0:
+        return jsonify({"respuesta": f"No es posible hacer una división por 0"})
+
+    resp = num1 / num2
+
+    return jsonify({"respuesta": f"El resultado de la división es: {resp}"})
 
 if __name__ == '__main__':
     app.run(debug=True)
